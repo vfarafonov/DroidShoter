90b0ad3b4a1f5ac821845f5d6dcf55423fda8d72
Vladimir Farafonov <vfarafonov@weezlabs.com>
2016-02-15 16:07:13 +0300

    Implemented switching states feature

--- src/com/weezlabs/tools/android/screenshoter/ScreenshoterMainScreen.java
+++ src/com/weezlabs/tools/android/screenshoter/ScreenshoterMainScreen.java
@@ -40,9 +40,11 @@ public class ScreenshoterMainScreen {
 	private JTextField prefixTextField;
 	private JTextField sleepTextField;
 	private JTextArea deviceParamsTextArea;
+	private MainScreenStates currentState_;
 
 	public ScreenshoterMainScreen() {
 		// TODO: add devices listener
+		setState(MainScreenStates.CONNECTING_ADB);
 		ScreenShooterManager.getInstanceAsync(new ScreenShooterManager.ManagerInitListener() {
 			@Override
 			public void onManagerReady(ScreenShooterManager manager) {
@@ -52,9 +54,11 @@ public class ScreenshoterMainScreen {
 				devicesListRenderer_.setAdbConnected_(true);
 				deviceInfoProgressBar.setVisible(false);
 				if (devices.length > 0) {
+					setState(MainScreenStates.REQUESTING_INFO);
 					screenShooterManager_.setDevice(new Device((IDevice) devicesComboBox.getSelectedItem()));
-					resetButton.setEnabled(true);
 					getSelectedDeviceInfo();
+				} else {
+					setState(MainScreenStates.DEVICE_NOT_FOUND);
 				}
 			}
 		});
@@ -65,6 +69,8 @@ public class ScreenshoterMainScreen {
 			@Override
 			public void itemStateChanged(ItemEvent e) {
 				if (e.getStateChange() == ItemEvent.SELECTED) {
+					deviceParamsTextArea.setText(null);
+					setState(MainScreenStates.REQUESTING_INFO);
 					getDeviceInfo((IDevice) e.getItem());
 				}
 			}
@@ -77,8 +83,8 @@ public class ScreenshoterMainScreen {
 		startButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (screenShooterManager_ != null){
-					switchUIForJobInProgress(true);
+				if (screenShooterManager_ != null) {
+					setState(MainScreenStates.JOB_IN_PROGRESS);
 					screenShooterManager_.createScreenshotsForAllResolutionsAsync(
 							new File(directoryTextField.getText()),
 							prefixTextField.getText(),
@@ -86,22 +92,16 @@ public class ScreenshoterMainScreen {
 							new ScreenShooterManager.ScreenShotJobProgressListener() {
 								@Override
 								public void onScreenshotJobFinished() {
-									System.out.println("Screen job finished");
-									switchUIForJobInProgress(false);
 									resetDeviceDisplay();
 								}
 
 								@Override
 								public void onScreenshotJobFailed() {
-									System.out.println("Screen job failed");
-									switchUIForJobInProgress(false);
 									resetDeviceDisplay();
 								}
 
 								@Override
 								public void onScreenshotJobCancelled() {
-									System.out.println("Screen job cancelled");
-									switchUIForJobInProgress(false);
 									resetDeviceDisplay();
 								}
 							}
@@ -112,8 +112,7 @@ public class ScreenshoterMainScreen {
 		cancelButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				System.out.println("cancel clicked");
-				cancelButton.setEnabled(false);
+				setState(MainScreenStates.CANCELLING_JOB);
 				screenShooterManager_.stopScreenshotsJob();
 			}
 		});
@@ -121,29 +120,6 @@ public class ScreenshoterMainScreen {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				resetDeviceDisplay();
-			}
-		});
-	}
-
-	private void switchUIForJobInProgress(boolean isInProgress) {
-		startButton.setEnabled(!isInProgress);
-		cancelButton.setEnabled(isInProgress);
-		resetButton.setEnabled(!isInProgress);
-	}
-
-	private void resetDeviceDisplay() {
-		resetButton.setEnabled(false);
-		screenShooterManager_.resetDeviceDisplayAsync(new ScreenShooterManager.CommandStatusListener() {
-			@Override
-			public void onCommandSentToDevice() {
-				System.out.println("reset finished");
-				resetButton.setEnabled(true);
-			}
-
-			@Override
-			public void onCommandExecutionFailed() {
-				System.out.println("reset failed");
-				resetButton.setEnabled(true);
 			}
 		});
 	}
@@ -156,6 +132,27 @@ public class ScreenshoterMainScreen {
 		frame.setVisible(true);
 	}
 
+	private void switchUIForJobInProgress(boolean isInProgress) {
+		startButton.setEnabled(!isInProgress);
+		cancelButton.setEnabled(isInProgress);
+		resetButton.setEnabled(!isInProgress);
+	}
+
+	private void resetDeviceDisplay() {
+		setState(MainScreenStates.RESETTING);
+		screenShooterManager_.resetDeviceDisplayAsync(new ScreenShooterManager.CommandStatusListener() {
+			@Override
+			public void onCommandSentToDevice() {
+				setState(checkGlobalState());
+			}
+
+			@Override
+			public void onCommandExecutionFailed() {
+				setState(checkGlobalState());
+			}
+		});
+	}
+
 	private void getSelectedDeviceInfo() {
 		IDevice iDevice = (IDevice) devicesComboBox.getSelectedItem();
 		if (iDevice != null) {
@@ -164,30 +161,90 @@ public class ScreenshoterMainScreen {
 	}
 
 	private void getDeviceInfo(@NonNull IDevice iDevice) {
-		devicesComboBox.setEnabled(false);
-		deviceParamsTextArea.setVisible(false);
-		deviceInfoProgressBar.setVisible(true);
 		ScreenShooterManager.getDeviceDisplayInfoAsync(iDevice, new ScreenShooterManager.DeviceInfoListener() {
 			@Override
 			public void onDeviceInfoUpdated(Device device) {
 				screenShooterManager_.setDevice(device);
 				deviceParamsTextArea.setText(device.getCurrentResolution().toString() + ", " + device.getCurrentDpi().toString());
-				deviceInfoProgressBar.setVisible(false);
-				deviceParamsTextArea.setVisible(true);
-
-				startButton.setEnabled(true);
-				resetButton.setEnabled(true);
-
-				devicesComboBox.setEnabled(true);
+				setState(checkGlobalState());
 			}
 
 			@Override
 			public void onDeviceUpdateFailed(IDevice iDevice, Exception e) {
 				deviceParamsTextArea.setText("Failed to get device parameters");
-				deviceInfoProgressBar.setVisible(false);
-				deviceParamsTextArea.setVisible(true);
-				devicesComboBox.setEnabled(true);
+				setState(checkGlobalState());
 			}
 		});
+	}
+
+	private MainScreenStates checkGlobalState() {
+		if (screenShooterManager_ == null) {
+			return MainScreenStates.CONNECTING_ADB;
+		} else if (screenShooterManager_.getDevices().length == 0) {
+			return MainScreenStates.DEVICE_NOT_FOUND;
+		} else if (deviceParamsTextArea.getText().equals("")) {
+			return MainScreenStates.REQUESTING_INFO;
+		} else {
+			return MainScreenStates.READY;
+		}
+	}
+
+	private void setState(MainScreenStates newState) {
+		currentState_ = newState;
+		switch (newState) {
+			case CONNECTING_ADB:
+				startButton.setEnabled(false);
+				cancelButton.setEnabled(false);
+				resetButton.setEnabled(false);
+				devicesComboBox.setEnabled(false);
+				deviceParamsTextArea.setVisible(false);
+				deviceInfoProgressBar.setVisible(true);
+				break;
+			case DEVICE_NOT_FOUND:
+				startButton.setEnabled(false);
+				cancelButton.setEnabled(false);
+				resetButton.setEnabled(false);
+				devicesComboBox.setEnabled(false);
+				deviceParamsTextArea.setVisible(true);
+				deviceInfoProgressBar.setVisible(false);
+				break;
+			case REQUESTING_INFO:
+				startButton.setEnabled(false);
+				cancelButton.setEnabled(false);
+				resetButton.setEnabled(true);
+				devicesComboBox.setEnabled(false);
+				deviceParamsTextArea.setVisible(false);
+				deviceInfoProgressBar.setVisible(true);
+				break;
+			case READY:
+				startButton.setEnabled(true);
+				cancelButton.setEnabled(false);
+				resetButton.setEnabled(true);
+				devicesComboBox.setEnabled(true);
+				deviceParamsTextArea.setVisible(true);
+				deviceInfoProgressBar.setVisible(false);
+				break;
+			case JOB_IN_PROGRESS:
+				startButton.setEnabled(false);
+				cancelButton.setEnabled(true);
+				resetButton.setEnabled(false);
+				devicesComboBox.setEnabled(false);
+				deviceParamsTextArea.setVisible(true);
+				deviceInfoProgressBar.setVisible(false);
+				break;
+			case CANCELLING_JOB:
+			case RESETTING:
+				startButton.setEnabled(false);
+				cancelButton.setEnabled(false);
+				resetButton.setEnabled(false);
+				devicesComboBox.setEnabled(false);
+				deviceParamsTextArea.setVisible(true);
+				deviceInfoProgressBar.setVisible(false);
+				break;
+		}
+	}
+
+	public enum MainScreenStates {
+		CONNECTING_ADB, DEVICE_NOT_FOUND, REQUESTING_INFO, JOB_IN_PROGRESS, CANCELLING_JOB, RESETTING, READY
 	}
 }
