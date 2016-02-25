b9aa5bd9eb279345742a743942b09fdb7f438cbe
Vladimir Farafonov <vfarafonov@weezlabs.com>
2016-02-25 16:51:37 +0300

    Changed app name. Created app icons

--- src/com/weezlabs/tools/android/screenshoter/ScreenshoterMainScreen.java
+++ src/com/weezlabs/tools/android/screenshoter/ScreenshoterMainScreen.java
@@ -10,14 +10,18 @@ import com.weezlabs.libs.screenshoter.model.Mode;
 import com.weezlabs.tools.android.screenshoter.ui.DevicesListRenderer;
 import com.weezlabs.tools.android.screenshoter.ui.ModesTableModel;
 
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -151,11 +155,25 @@ public class ScreenshoterMainScreen {
 	}
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("ScreenShoter");
+		JFrame frame = new JFrame("DroidShoter");
+		frame.setIconImages(getIconsList(frame.getClass()));
 		frame.setContentPane(new ScreenshoterMainScreen().ScreenshoterRootPanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
+	}
+
+	private static ArrayList<Image> getIconsList(Class<?> aClass) {
+		ArrayList<Image> iconsList = new ArrayList<>();
+		try {
+			iconsList.add(ImageIO.read(aClass.getResource("/res/images/icon_128.png")));
+			iconsList.add(ImageIO.read(aClass.getResource("/res/images/icon_64.png")));
+			iconsList.add(ImageIO.read(aClass.getResource("/res/images/icon_32.png")));
+			iconsList.add(ImageIO.read(aClass.getResource("/res/images/icon_16.png")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return iconsList;
 	}
 
 	/**
